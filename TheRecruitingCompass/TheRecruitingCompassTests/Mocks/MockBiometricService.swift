65523418ab7b81bb4b25a780d38c3c54088ed03f
Chris Andrikanich <chris@andrikanich.com>
2026-03-08 09:57:25 -0400

    test(biometric): add MockBiometricService

---
+++ TheRecruitingCompass/TheRecruitingCompassTests/Mocks/MockBiometricService.swift
@@ -0,0 +1,17 @@
+@testable import TheRecruitingCompass
+
+@MainActor
+final class MockBiometricService: BiometricServiceProtocol {
+  var canEvaluateResult = true
+  var authenticateResult: Result<Void, Error> = .success(())
+  var authenticateCallCount = 0
+
+  func canEvaluateBiometrics() -> Bool {
+    canEvaluateResult
+  }
+
+  func authenticate(reason: String) async throws {
+    authenticateCallCount += 1
+    try authenticateResult.get()
+  }
+}
