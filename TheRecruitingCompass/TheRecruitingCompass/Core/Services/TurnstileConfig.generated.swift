5028bfa685e5b6ab5b25d14cfa7f99b9452d7f8d
Chris Andrikanich <chris@andrikanich.com>
2026-09-09 09:03:21 -0400

    chore(config): add TURNSTILE_SITE_KEY to generated-config build phase

---
+++ TheRecruitingCompass/TheRecruitingCompass/Core/Services/TurnstileConfig.generated.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+/// Auto-generated at build time from Release.xcconfig. Do not edit manually.
+enum TurnstileConfigEmbedded {
+  static let siteKey = "0x4AAAAAAEc9xAHjjO-Jv5pg"
+}
